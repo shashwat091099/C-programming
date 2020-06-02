0021a824950dcda9d9a12efcb0f1100f2dffc94a
Coursera Learner <nobody@nowhere.nul>
2020-06-02 19:08:34 +0000

    Did assingment 03 of 03_code2

--- 03_code2/code2.c
+++ 03_code2/code2.c
@@ -1,22 +1,22 @@
 
 int printTriangle(int size) {
-  //start with starCount being 0
+  int starCount=0,i=1,j;  //start with starCount being 0
 
-  //count from 0 (inclusive) to size (exclusive), for each number i that you count
+  while(i<=size)  //count from 0 (inclusive) to size (exclusive), for each number i that you count
+    {  j=1;
+      while(j<=i) //count from 0 (inclusive) to i (inclusive), for each number j that you count
+	{
+	  printf("*"); //print a "*"
+	  j++;
+	  starCount++; //increment starCount
 
-     //count from 0 (inclusive) to i (inclusive), for each number j that you count
+	} //when you finish counting on j, 
 
-        //print a "*"
+      printf("\n");  //print a newline ("\n")
+      i++;
+    } //when you finish counting on i, 
 
-        //increment starCount
-
-     //when you finish counting on j, 
-
-     //print a newline ("\n")
-
-  //when you finish counting on i, 
-
-  //your answer is starCount
+  return starCount; //your answer is starCount
 
 }
 
@@ -26,11 +26,11 @@ int main(void) {
   printf("Here is a triangle with height 4\n");
   numStars = printTriangle(4);
   printf("That triangle had %d total stars\n", numStars);
-  //now print "Here is a triangle with height 7\n"
+  printf("Here is a triangle with height 7\n");  //now print "Here is a triangle with height 7\n"
 
-  //then call printTriangle, passing in 7, and assign the result to numStars
+  numStars = printTriangle(7); //then call printTriangle, passing in 7, and assign the result to numStars
 
-  //finally, print "That triangle had %d total stars\n", such that the %d 
+  printf("That triangle had %d total stars\n",numStars);//finally, print "That triangle had %d total stars\n", such that the %d 
   //prints the value of numStars
 
 
