88aaba1b8ea80f61fe73095d4dc4c56f60656ec6
Coursera Learner <nobody@nowhere.nul>
2020-07-03 09:29:04 +0000

    Did assignment

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -3,193 +3,162 @@
 #include <assert.h>
 #include "deck.h"
 
+int com (card_t c1,card_t c2){
+  if ((c1.value == c2.value) &&(c1.suit == c2.suit)) return 1;
+  return 0;
+
+}
+
 void print_hand(deck_t * hand){
-  //use printcard fxn i've already written
-  //deck_t has card_t **cards and size_t n_cards
-  //card_t has 2<=value<=14 and 0<=suit<=4
-  for (int i = 0; i < (hand->n_cards); i++){
-    print_card(*(hand->cards[i])); //dereference cards?
-    printf(" ");
+  card_t ** card =hand -> cards ;
+  card_t  card1;
+  for (size_t i=0 ;i<(hand -> n_cards );i++){
+    card1=**(card +i);
+    print_card(card1);
   }
 }
 
 int deck_contains(deck_t * d, card_t c) {
-  //search a deck for a particular card
-  //deck_t has card_t **cards and size_t n_cards
-  //card_t has 2<=value<=14 and 0<=suit<=4
-  for (int i = 0; i < (d->n_cards); i++){
-    if (( (d->cards[i]->value) == c.value) && ( (d->cards[i]->suit) == c.suit)){//is this how you index an array of structs?
-      return 1;
-    }
+  card_t ** card =d -> cards ;
+  for (size_t i=0 ;i< d -> n_cards ;i++){
+    if (com(**(card+i),c)) return 1;
   }
+
   return 0;
 }
 
 void shuffle(deck_t * d){
-  //random swapping
-  //swap i with random j in the deck for each i
-  card_t swap;
-  int swapIndex;
-  for (int i = 0; i < (d->n_cards); i++){//does this need double indexing?
-    swap = *(d->cards[i]);
-    swapIndex = random()%(d->n_cards);//generates a random number from 0 to #cards in deck
-    *(d->cards[i]) = *(d->cards[swapIndex]);
-    *(d->cards[swapIndex]) = swap;
+  card_t ** card =d -> cards ;
+  card_t * temp;
+  size_t n=d ->n_cards;
+  int randarry;
+  for (size_t i=0 ;i< n/2 ;i++){
+    randarry= random()%(n-i)+i;
+    temp=card[i];
+    card[i]=card[randarry];
+    card[randarry]=temp;
   }
 }
 
 void assert_full_deck(deck_t * d) {
-  //check that each card only appears once per deck
-
-  //check that the lengths of the decks are the same length
-  assert((d->n_cards) = 52);
-
-  //use deck contains and card_from_num fxn to check how many times a card appears in the deck
+  card_t ** card =d -> cards ;
+  card_t c;
   int count;
-  card_t temp;
-  for (int i = 0; i < 52; i++){ //scroll through list of possible cards
-    for (int j = 0; j < (d->n_cards); j++){ //scroll through input deck
-      temp = card_from_num(i);
-      if ( ( (d->cards[j]->value) == temp.value) && ( (d->cards[j]->suit) == temp.suit)){
-	count++;
-      }
+
+  for (size_t i=0 ;i< d -> n_cards ;i++){
+    c=**(card+i);
+    count=0;
+    for (size_t j=0 ;j< d -> n_cards ;j++){
+      if(com(**(card+j), c)) count ++;
     }
-    //if card occurs more than once return error
-    if (count > 1){
-      printf("ERROR: deck has repeating card ");
-      print_card(temp);
-      printf("\n");
-      return;
-    }
-    else{
-      count = 0;
-    }
+    assert(count ==1);
   }
 }
 
-//======PROJECT 4 CODE===========
+void add_card_to(deck_t * deck, card_t c){
 
-void add_card_to( deck_t * deck, card_t c){
-  //add a particular card to a given deck (realloc)
-  //deck_t has card_t **cards, size_t n_cards
-  //card_t has uint value, suit_t suit
-  card_t ** cards = malloc(sizeof(card_t*));
-  cards[0] = malloc(sizeof(card_t));
-  *cards[0] = c;
+  card_t* c1=malloc(sizeof(*c1));
 
-  int newLen = deck->n_cards + 1;
-  deck->cards = realloc(deck->cards, newLen*sizeof(card_t *));
-  deck->cards[newLen-1] = cards[0];
-  //printf("added %d: value: %d, suit: %d\n", newLen-1, deck->cards[deck->n_cards]->value, deck->cards[deck->n_cards]->suit);
-  deck->n_cards++;
+  c1->value = c.value;
+
+  c1->suit = c.suit;
+
+  deck ->cards= realloc(deck->cards,(deck ->n_cards + 1)*sizeof(*deck ->cards));
+
+  deck ->cards[deck ->n_cards]=c1;
+
+  deck ->n_cards ++;
+
 }
+
+
 
 card_t * add_empty_card(deck_t * deck){
-  //adds an empty card (value 0, suit 0) to a deck and returns a pointer to it
-  static card_t empty;
-  empty.value = 0;
-  empty.suit = NUM_SUITS;
-  add_card_to(deck, empty);
-  card_t * ans = deck->cards[deck->n_cards-1];
-  return ans;
+
+  card_t* c1=malloc(sizeof(*c1));
+
+  c1->value = 0;
+
+  c1->suit = 0;
+
+  deck ->cards= realloc(deck->cards,(deck ->n_cards + 1)*sizeof(*deck ->cards));
+
+  deck ->cards[deck ->n_cards]=c1;
+
+  deck ->n_cards ++;
+
+  return c1;
+
 }
 
 
-void printDeck(deck_t * deck){
-  printf("\n");
-  for ( int i = 0; i < deck->n_cards; i++){
-    print_card(*(deck->cards[i]));
-    printf(" ");
-  }
-  printf("\n");
-}
+
+
 
 deck_t * make_deck_exclude(deck_t * excluded_cards){
-  //creates a full deck minus the cards in excluded_cards
-  //use card_from_num and deck_contains
-  //0-12 hearts, 13-25 diamonds, 26-38 clubs, 39-51 spades
-  //  0. allocate the card_t deck and each card object in it
-  //  1. allocate the deck struct
-  //  2. allocate the cards array in the deck
-  //  3. allocate each card in the array
-  int len = 52 - excluded_cards->n_cards;
 
-  card_t ** cards = malloc(len*sizeof(card_t*));
+  deck_t* ans=malloc(sizeof(*ans));
 
-  deck_t * ans = malloc(sizeof(deck_t));
-  ans->cards = malloc(len*sizeof(card_t*));
+  ans->cards=NULL;
 
-  int k = 0;
-  for ( int i = 0; i < 13; i++ ){
-    for ( int j = 0; j < 4; j++ ){
-      if ( deck_contains(excluded_cards, card_from_num(i+13*j)) == 0){
-	cards[k] = malloc(sizeof(card_t));
-	*cards[k] = card_from_num(i+13*j);
-	ans->cards[k] = cards[k];
-	k++;
-      }
-    }
+  ans->n_cards=0;
+
+  for(unsigned i=0;i<52;i++){
+
+    card_t c= card_from_num(i);
+
+    if(!deck_contains(excluded_cards,c)) add_card_to(ans,c);
+
   }
 
-  ans->n_cards = len;
   return ans;
+
 }
 
-deck_t * build_remaining_deck( deck_t ** hands, size_t n_hands){
-  //build the deck that remains once the hands have been dealt
-  //find total number of cards in all hands
-  //  0. allocate the card_t deck and each card object in it (done before hands??)
-  //     hands is an array of pointers to deck that point to arrays of card_t pointers
-  //  1. allocate the deck struct
-  //  2. allocate the cards array in the deck
-  //  3. allocate each card in the array
+deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands) {
 
-  //ans is a one element array where the one element is a struct holidng all of the cards
-  //  from the hands array of pointers to deck_t structs
+  deck_t* deck=malloc(sizeof(*deck));
 
-  deck_t * all = malloc(sizeof(deck_t));
-  int len = 0;
+  deck->n_cards=0;
 
-  for (int i = 0; i < n_hands; i++){
-    len+= hands[i]->n_cards;
-  }
+  deck->cards=NULL;
 
-  card_t ** cards = malloc(len*sizeof(card_t*));
+  for (size_t i=0 ; i<n_hands ;i++){
 
-  all->cards = malloc(len*sizeof(card_t*));
-  all->n_cards = len;
+    for(int x=0 ; x<hands[i]->n_cards ;x++){
 
-  int k = 0;
-  for (int i = 0; i < n_hands; i++){
-    for (int j = 0; j < hands[i]->n_cards; j++){
-      cards[k] = malloc(sizeof(card_t));
-      *cards[k] = *(hands[i]->cards[j]);
-      all->cards[k] = cards[k];
-      k++;
+      deck->cards=realloc(deck->cards,(deck->n_cards+1)*sizeof(*deck->cards));
+
+      deck->cards[deck->n_cards]=hands[i]->cards[x];
+
+      deck->n_cards ++;
+
     }
+
   }
 
-  printDeck(all);
+  deck_t* deck2=make_deck_exclude(deck);
 
-  deck_t * ans = make_deck_exclude(all);
-  free_deck(all);
-  /*
-  for ( int i = 0; i < len; i++){
-    free(cards[i]);
-  }
-  free(cards);
-  */
-  return ans;
-}
-
-void free_deck(deck_t * deck){
-  //free(*(deck->cards));
-  for ( int i = 0; i < deck->n_cards; i++){
-    free((deck->cards[i]));
-    //deck->cards[i] = NULL;
-  }
   free(deck->cards);
-  //deck->cards = NULL;
+
   free(deck);
-  //deck = NULL;
+
+  return deck2 ;
+
+}
+
+
+
+void free_deck(deck_t * deck) {
+
+  for(int i=0 ;i<deck->n_cards;i++){
+
+    free(deck->cards[i]);
+
+  }
+
+  free(deck->cards);
+
+  free(deck);
+
 }
